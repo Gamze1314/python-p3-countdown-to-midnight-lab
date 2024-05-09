5f2e49d01048d07c11ea37727539fa9b2234b016
gamze <gamzekandara@gmail.com>
2024-05-09 13:26:55 -0400

    Adds countdown.py

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
-# your code goes here!
+import time
+def countdown(int):
+    while True:
+        print(int)
+        int -= 1
+        if int == 0:
+            print("HAPPY NEW YEAR!")
+            break
+
+
+def countdown_with_sleep(int):
+    while True:
+        print(int)
+        int -= 1
+        if int == 0:
+            print("HAPPY NEW YEAR!")
+            break
+        time.sleep(1)
+
+countdown_with_sleep(10)
